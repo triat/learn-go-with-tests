925976b9153dea6f2e518114f027a1b0113f53e7
Tom Riat <tom@riat.dev>
2023-08-17 08:45:34 +0200

    feat: 04 array and slice

--- 04-array-slice/sum_test.go
+++ 04-array-slice/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Collection of 5 numbers", func(t *testing.T) {
@@ -22,5 +25,36 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("expected %d, got %d, given %v", want, got, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("expected %v got %v", want, got)
+	}
+}
+
+func TestSumTail(t *testing.T) {
+	checkSum := func(t testing.TB, got, want []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected %v got %v", want, got)
+		}
+	}
+	t.Run("make the sums of some slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSum(t, got, want)
+	})
+	t.Run("make the sums of an empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSum(t, got, want)
 	})
 }
